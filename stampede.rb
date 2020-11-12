c205befaef2da6e11f8b3d2b58aa5ac13a18430b
bashovski <anurbasic01@outlook.com>
2020-11-13 00:23:19 +0100

    Add brew formula for CLI

---
+++ stampede.rb
@@ -0,0 +1,12 @@
+class StampedeCli < Formula
+  desc "CLI for Stampede Framework"
+  homepage ""
+  url "https://github.com/bashovski/stampede-cli/archive/alpha.tar.gz"
+  version "alpha"
+  sha256 ""
+  license "MIT License"
+
+  def install
+    bin.install "stampede"
+  end
+end
